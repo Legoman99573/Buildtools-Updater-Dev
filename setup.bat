723820a65c37df649ee76d5e5742fcce5a8e8348
Legoman99573 <christianwhead@gmail.com>
2016-04-01 14:39:57 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,30 +1,37 @@
 @echo off
 
-@echo Generating config/version.txt
+if exist config/version.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/version.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/version.txt -OutFile config/version.txt
+)
 
-@echo Generating config/gitlocation.txt
+if exist config/version.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/gitlocation.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/gitlocation.txt -OutFile config/gitlocation.txt
+)
 
-@echo Generating config/plugin.txt
+if exist config/version.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/plugin.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/plugin.txt -OutFile config/plugin.txt
+)
 
 @echo Finished Generating config Folder
 
 @echo Generating necessary folders
 @echo Generating folder /plugin-pending
-md plugin-pending
+if exist plugin-pending/ (@echo /plugin-pending exists. This can be ignored) else (md plugin-pending)
 
 @echo Generating folder /plugin-fixed
-md plugin-fixed
+if exist plugin-fixed/ (@echo /plugin-pending exists. This can be ignored) else (md plugin-fixed)
 
 @echo Generating folder /bungee
-md bungee
-md bungee/modules
+if exist bungee/ (@echo /bungee exists. This can be ignored) else (md bungee)
+if exist bungee/modules/ (@echo /bungee/modules exists. This can be ignored) else (md bungee/modules)
 
 @echo Generating folder /api
-md api
+if exist api/ (@echo /bungee exists. This can be ignored) else (md api)
 
 @echo finished Generating all folders
+
+@echo Updating update.bat
+del /f update.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/Legoman99573/Buildtools-Updater-Dev/master/update.bat -OutFile update.bat
 
 exit
