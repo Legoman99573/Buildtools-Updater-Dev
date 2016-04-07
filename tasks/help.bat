be17ecba5aace79364b20bf02295ebee70e9b893
Legoman99573 <christianwhead@gmail.com>
2016-04-07 17:06:29 -0600

    changed from help menu to command list

--- tasks/help.bat
+++ tasks/help.bat
@@ -1,37 +1,37 @@
 @echo off
 echo.
-@echo ================ {Help Menu} ================
+@echo ================ {Command List} ================
 echo.
 @echo update: Updates Buildtools to latest version
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
-@echo help: Displays help menu
+@echo help: Just added it if you want to spam it lol
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo run: Runs Buildtools and saves API's
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo bungee: Updates BungeeCord and its modules
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo clean: Deletes Buildtools files.
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo plugin: Runs Plugin-Fixer
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo btupdate: Updates this script
 echo.
-@echo ---------------------------------------------
+@echo ------------------------------------------------
 echo.
 @echo exit: Shuts Down Program
 echo.
-@echo ================ {Help Menu} ================
+@echo ================ {Command List} ================
 echo.
 exit
