d73075fb3173e9c05127927e0d6c147cfa3e3913
Legoman99573 <christianwhead@gmail.com>
2016-04-07 16:44:19 -0600

    updated to move files to new directory if using old version

--- tasks/setup.bat
+++ tasks/setup.bat
@@ -1,17 +1,25 @@
 @echo off
 
+if exist tasks/setup.bat (goto run) else (exit)
+
+:run
+
 if exist config/ (@echo Found config folder) else (md config)
 
 if exist config/version.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/version.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/version.txt -OutFile config/version.txt
 )
 
-if exist config/gitlocation.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/gitlocation.txt
+if exist config/gitlocation.txt (@echo gitlocation.txt exists. This can be ignored) else (@echo Generating config/gitlocation.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/gitlocation.txt -OutFile config/gitlocation.txt
 )
 
-if exist config/plugin.txt (@echo version.txt exists. This can be ignored) else (@echo Generating config/plugin.txt
+if exist config/plugin.txt (@echo plugin.txt exists. This can be ignored) else (@echo Generating config/plugin.txt
 powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/plugin.txt -OutFile config/plugin.txt
+)
+
+if exist config/autoupdate.txt (@echo autoupdate.txt exists. This can be ignored) else (@echo Generating config/autoupdate.txt
+powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/autoupdate.txt -OutFile config/autoupdate.txt
 )
 
 @echo Finished Generating config Folder
@@ -50,6 +58,18 @@ If exist tasks/CraftBukkit/ (move tasks\CraftBukkit tasks\Buildtools_Files\)
 If exist tasks/Spigot/ (move tasks\Spigot tasks\Buildtools_Files\)
 
 If exist tasks/work/ (move tasks\work tasks\Buildtools_Files\)
+
+If exist update.bat (move update.bat tasks\)
+
+If exist plugin.bat (move plugin.bat tasks\)
+
+If exist help.bat (move help.bat tasks\)
+
+if exist license/ (@echo /license exists. This can be ignored) else (md license)
+
+If exist license.file (move license.file license\)
+
+If exist README.md (move README.md license\)
 
 @echo Updating update.bat
 del /f update.bat
